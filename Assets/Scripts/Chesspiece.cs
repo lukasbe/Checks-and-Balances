87b16e8872dcfd575d28aadd16b7a64b80b2b221
Lukas Berger <lukas.berger@outlook.at>
2017-12-02 22:49:04 +0100

    merge finalRotation

--- Assets/Scripts/Chesspiece.cs
+++ Assets/Scripts/Chesspiece.cs
@@ -26,39 +26,55 @@ public abstract class Chesspiece : MonoBehaviour
 		RenderWeights();
 	}
 
-	private void CalculateHeight()
+	private void CalculateHeight ()
 	{
-//		this.height = this.normalizedWeight * weightPrefab.transform.localScale.y;
-        // Limit weight rendering to 10
-        // Mathf because Math does not exist in unity
+		Vector3 up = MoveHighlights.moveHighlights [CurrentX, CurrentY].transform.up;
+		Vector3 tileCenter = GetTileCenter (CurrentX, CurrentY);
 
-        Vector3 tileCenter = MoveHighlights.Instance.GetTileCenter(CurrentX, CurrentY);
-        tileCenter.y += this.weight * (weightPrefab.transform.localScale.z * 2);
-        transform.position = tileCenter;
+		float height = (weightPrefab.transform.localScale.z * 2) * this.weight;
+		Vector3 target = tileCenter + (up * height);
+		Vector3 endTarget = target + (up * weightPrefab.transform.localScale.z);
+		Vector3 end = Vector3.MoveTowards (target, endTarget, 1.0f);
+
+		transform.position = target;
+		Quaternion localRot = transform.localRotation;
+		localRot.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+		transform.localRotation = localRot;
+
 	}
 
-	private void RenderWeights()
+	private void RenderWeights ()
 	{
-		
-        // Remove all current Weight
+
+		// Remove all current Weight
 		foreach (Transform child in transform)
-        {
+		{
 			GameObject.Destroy (child.gameObject);
-        }
+		}
 
-        // Limit weight rendering to 10
-        // Mathf because Math does not exist in unity
+		//upo vector
+		Vector3 up = MoveHighlights.moveHighlights [CurrentX, CurrentY].transform.up;
 
+		//start vectors
+		Vector3 tileCenter = GetTileCenter (CurrentX, CurrentY);
+		Vector3 startTarget = tileCenter + (up * weightPrefab.transform.localScale.z);
+		Vector3 start = Vector3.MoveTowards (tileCenter, startTarget, 1.0f);
+
+
+		float height = (weightPrefab.transform.localScale.z * 2) * this.weight;
+		Vector3 target = tileCenter + (up * height);
+		Vector3 endTarget = target + (up * weightPrefab.transform.localScale.z);
+		Vector3 end = Vector3.MoveTowards (target, endTarget, 1.0f);
+
+		float step = height / this.weight;
 
 		for (int i = 0; i < this.weight; i++) {
-			Vector3 tileCenter = MoveHighlights.Instance.GetTileCenter (CurrentX, CurrentY);
-			tileCenter.y += weightPrefab.transform.localScale.z;
-			tileCenter.y += i * (weightPrefab.transform.localScale.z * 2);
 			GameObject go = Instantiate (weightPrefab) as GameObject;
-			go.transform.position = tileCenter;
-			go.transform.up = MoveHighlights.moveHighlights [CurrentX, CurrentY].transform.up;
-			go.transform.Rotate(new Vector3(90.0f, 0.0f, 0.0f));
 			go.transform.SetParent (transform);
+			go.transform.position = Vector3.MoveTowards (start, end, i * step);
+			Quaternion localRot = go.transform.localRotation;
+			localRot.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+			go.transform.localRotation = localRot;
 		}
 	}
 
