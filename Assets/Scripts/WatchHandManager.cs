22482cc457c37e027f8b2805d552d4af516c4515
Lukas Berger <lukas.berger@outlook.at>
2017-12-03 23:10:18 +0100

    texture

--- Assets/Scripts/WatchHandManager.cs
+++ Assets/Scripts/WatchHandManager.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class WatchHandManager : MonoBehaviour {
 
+	GameObject chessBoard;
+
 	// Use this for initialization
 	void Start () {
-		
+		chessBoard = GameObject.Find ("chessboard");
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		//transform.rotation = Quaternion.Euler (-90.0f, 0.0f, 0.0f);
+		transform.up = chessBoard.transform.up;
+		transform.Rotate (90.0f, 0.0f, 0.0f);
 	}
 }
